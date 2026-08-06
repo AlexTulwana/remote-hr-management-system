14e3a5972c1978870d64b4a03a222c9f0a8cd500
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-06 23:04:26 +0200

    Recreate missing ApplicationRepository
    This file didn't exist in the repo, which was silently breaking the build.

--- backend/src/main/java/com/wethinkcode/hrsystem/repository/ApplicationRepository.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/repository/ApplicationRepository.java
@@ -1,4 +1,12 @@
 package com.wethinkcode.hrsystem.repository;
 
-public class ApplicationRepository {
+import com.wethinkcode.hrsystem.model.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    List<Application> findByJobPostingId(Long jobPostingId);
+    List<Application> findByStatus(String status);
+    long countByJobPostingId(Long jobPostingId);
 }
