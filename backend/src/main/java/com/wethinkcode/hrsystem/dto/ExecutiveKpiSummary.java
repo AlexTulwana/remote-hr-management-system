959febe447e0227058b5e74b3c78aebf195f5c68
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-21 20:48:04 +0200

    Add ExecutiveKpiSummary DTO for Executive KPIs endpoint

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/ExecutiveKpiSummary.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/ExecutiveKpiSummary.java
@@ -1,4 +1,16 @@
 package com.wethinkcode.hrsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ExecutiveKpiSummary {
+    private long totalEmployees;
+    private long activeEmployees;
+    private double turnoverRate30d;
+    private long totalPendingItems;
+    private long totalApplications;
+    private long hiredCount;
+    private double totalSalarySpend;
 }
