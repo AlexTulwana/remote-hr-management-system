cee019c6a7d118a2189e0244afacdd98678f7b2a
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-21 20:48:33 +0200

    Add EmployeeDashboardSummary DTO for Employee summary endpoint

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/EmployeeDashboardSummary.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/EmployeeDashboardSummary.java
@@ -1,4 +1,21 @@
 package com.wethinkcode.hrsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class EmployeeDashboardSummary {
+    private String employeeNumber;
+    private String fullName;
+    private String position;
+    private String department;
+    private String branchName;
+    private String employmentStatus;
+    private long pendingLeaveRequests;
+    private long approvedLeaveRequests;
+    private long rejectedLeaveRequests;
+    private long attendanceRecordsThisMonth;
+    private long pendingRequestsCount;
+    private long openDisciplinaryCasesCount;
 }
