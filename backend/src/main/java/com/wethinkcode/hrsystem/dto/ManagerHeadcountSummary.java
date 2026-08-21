294bcba1d331e914e03fdf280a33ab76dd2469a7
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-21 20:45:35 +0200

    Add ManagerHeadcountSummary DTO for Manager headcount endpoint

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/ManagerHeadcountSummary.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/ManagerHeadcountSummary.java
@@ -1,4 +1,14 @@
 package com.wethinkcode.hrsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
 public class ManagerHeadcountSummary {
+    private long totalEmployees;
+    private long activeEmployees;
+    private Map<String, Long> activeByDepartment;
 }
