7a659fc8758c8f610e29b0d4f8de43153922d90f
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-21 20:47:36 +0200

    Add TurnoverSummary DTO for HR turnover endpoint

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/TurnoverSummary.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/TurnoverSummary.java
@@ -1,4 +1,13 @@
 package com.wethinkcode.hrsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
 public class TurnoverSummary {
+    private LocalDate asOfDate;
+    private double turnoverRate30d;
 }
