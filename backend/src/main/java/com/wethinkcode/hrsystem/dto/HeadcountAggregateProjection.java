5bc6b1dcb2921e4f7ed99a82cbb4f96b82d0bd24
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-21 20:46:47 +0200

    Confirm HeadcountAggregateProjection interface fields

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/HeadcountAggregateProjection.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/HeadcountAggregateProjection.java
@@ -1,4 +1,8 @@
 package com.wethinkcode.hrsystem.dto;
 
-public class HeadcountAggregateProjection {
+public interface HeadcountAggregateProjection {
+    Long getBranchId();
+    String getDepartment();
+    String getEmploymentStatus();
+    Long getHeadcount();
 }
