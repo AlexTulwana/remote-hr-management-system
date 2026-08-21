b273351271e951dd2c00d45af378310c4ccc1e00
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-21 20:47:13 +0200

    Add HeadcountTrendPoint DTO for HR headcount trend endpoint

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/HeadcountTrendPoint.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/HeadcountTrendPoint.java
@@ -1,4 +1,13 @@
 package com.wethinkcode.hrsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
 public class HeadcountTrendPoint {
+    private LocalDate date;
+    private long totalActive;
 }
