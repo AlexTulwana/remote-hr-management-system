9582a1ee3ed96e572ba6cbc9e307f1a735cc298b
Alex Tulwana <Tulwanaoa@gmail.com>
2026-08-21 20:47:31 +0200

    Add HrPendingItemsSummary DTO aggregating leave, requests, disciplinary cases, applications

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/HrPendingItemsSummary.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/HrPendingItemsSummary.java
@@ -1,4 +1,16 @@
 package com.wethinkcode.hrsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
 public class HrPendingItemsSummary {
+    private List<LeaveRequestSummary> pendingLeave;
+    private List<EmployeeRequestSummary> pendingRequests;
+    private List<DisciplinaryCaseSummary> openDisciplinaryCases;
+    private long pendingApplications;
+    private long totalPendingCount;
 }
