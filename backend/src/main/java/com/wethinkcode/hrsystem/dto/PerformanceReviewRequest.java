732b47665de0fa96b3b04d2bda261f0bb8458247
Alex Tulwana <Tulwanaoa@gmail.com>
2026-09-21 02:40:37 +0200

    Remove reviewerId from PerformanceReviewRequest

--- backend/src/main/java/com/wethinkcode/hrsystem/dto/PerformanceReviewRequest.java
+++ backend/src/main/java/com/wethinkcode/hrsystem/dto/PerformanceReviewRequest.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class PerformanceReviewRequest {
     private Long employeeId;
-    private Long reviewerId;
     private int communicationScore;
     private int teamworkScore;
     private int productivityScore;
