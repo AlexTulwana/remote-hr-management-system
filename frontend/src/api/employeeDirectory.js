ceafe1b8915dfd94a79f6045432789908bc8e2bb
Alex Tulwana <Tulwanaoa@gmail.com>
2026-09-23 14:17:10 +0200

    Add getOrgChart() for full company org chart

--- frontend/src/api/employeeDirectory.js
+++ frontend/src/api/employeeDirectory.js
@@ -4,6 +4,10 @@ export function getDirectory() {
   return apiFetch('/api/employee-directory');
 }
 
+export function getOrgChart() {
+  return apiFetch('/api/employee-directory/org-chart');
+}
+
 export function getOrgChartFrom(employeeId) {
   return apiFetch(`/api/employee-directory/org-chart/${employeeId}`);
 }
